434b7d25c744fbbb1840581b21438d66451a1b7d
片岡義森 <kataokayoshimori@kataokayoshimorinoMacBook-Pro.local>
2020-02-08 14:49:37 +0900

    rails5-13

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
 
   devise_for :users, :controllers => {:registrations => "registrations"}
   root to: 'pages#index'
+
+  devise_scope :user do
+    get 'login', to: 'devise/sessions#new'
+  end
   
 
   devise_scope :user do
